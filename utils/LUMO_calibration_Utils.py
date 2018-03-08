089d40010dd92b08dc298bb604cfc3c34759d0c9
elliott <e.l.warren@pgr.reading.ac.uk>
2018-03-08 15:58:17 +0000

    init commit

---
+++ utils/LUMO_calibration_Utils.py
@@ -0,0 +1,10 @@
+"""
+Functions to support the ceilometer calibration script (LUMO_ceil_block_calibration.py)
+
+Functions copied/tweaked from Emma Hopkin's set of Utility functions.
+
+LUMO_calibration_Utils created by Elliott Thurs 08/03/2018
+
+Original functions created by Emma Hopkin
+Edited by Elliott Warren
+"""
